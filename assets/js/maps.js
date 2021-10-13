fd64f2d11dfa5082842c45c3562f145bcee1dede
JakubKocerha <jakubbubble@gmail.com>
2021-10-13 17:08:36 +0000

    Adding markers into Google Maps.

--- assets/js/maps.js
+++ assets/js/maps.js
@@ -1,21 +1,98 @@
 function initMap() {
-    // Maps options
-    var options = {
-        zoom: 4,
+    // Maps initial options for different containers
+
+    // default map view of South America
+    var option1 = {
+        zoom: 3,
         center: {
-            lat: 42.0000,
+            lat: 11.852733,
             lng: -71.0000
         }
     }
-    // New map
-    var map = new google.maps.Map(document.getElementById('map'), options);
 
-    // Add marker
-    var marker = new google.maps.Marker({
+
+    // default map view of South-East Asia
+    var option2 = {
+        zoom: 3,
+        center: {
+            lat: 14.431932,
+            lng: 101.394869
+        }
+    }
+
+
+
+    // New map for each container
+    var mapA = new google.maps.Map(document.getElementById('map-a'), option1);
+    var mapB = new google.maps.Map(document.getElementById('map-b'), option2);
+
+
+
+
+    // Set the string of content into infoWindow
+    var contentStringA =
+        '<div>' +
+        '<h2>Philodendron White Knight</h2>' +
+        '<div>' +
+        '<p>This rare beauty is native to the tropical forests of South America</p>' +
+        '</div>' +
+        '</div>';
+
+    // Google Apis documentation - Setting infowndow with contentString variable
+    var infowindowA = new google.maps.InfoWindow({
+        content: contentStringA,
+    });
+    // Add marker for a map
+    var markerA = new google.maps.Marker({
         position: {
-            lat: 42.4668,
-            lng: -70.0000
+            lat: 11.852733,
+            lng: -71.0000
         },
-        map: map
+        map: mapA,
+        title: "Philodendron White Knight" // parameter of the map we have just created with var map
+        // map: other div element
+    });
+
+    // listener triggering onclick function, rendering infoWindow
+    markerA.addListener("click", () => {
+        infowindowA.open({
+            anchor: markerA,
+            map: mapA,
+            shouldFocus: false,
+        });
+    });
+
+
+
+    var contentStringB =
+        '<div>' +
+        '<h2>Monstera Thai Constellation</h2>' +
+        '<div>' +
+        '<p>As not in case of its relatives, this special beauty found its birth in a laboratory in Thailand</p>' +
+        '</div>' +
+        '</div>';
+
+    // Google Apis documentation - Setting infowndow with contentString variable
+    var infowindowB = new google.maps.InfoWindow({
+        content: contentStringB,
+    });
+    // Add marker for a map
+    var markerB = new google.maps.Marker({
+        position: {
+            lat: 14.431932,
+            lng: 101.394869
+        },
+        map: mapB,
+        title: "Mostera Thai Constellation" // parameter of the map we have just created with var map
+        // map: other div element
+    });
+
+    // listener triggering onclick function, rendering infoWindow
+    markerB.addListener("click", () => {
+        infowindowB.open({
+            anchor: markerB,
+            map: mapB,
+            shouldFocus: false,
+        });
     });
 }
